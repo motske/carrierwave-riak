57e1a4ce9f2ccc686705a397626a16399d2e6277
Zach Colon <zach.colon@tma1.com>
2013-03-27 11:29:54 -0400

    fixed test server bug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,21 +39,20 @@ RSpec.configure do |config|
   config.order = :random
 
   config.before :suite do
-    unless $test_server
-      begin
-        config = YAML.load_file('spec/support/test_server.yml')
-        test_server = Riak::TestServer.create(config.symbolize_keys)
-        test_server.start
-      rescue => e
-        warn "Can't run Riak::TestServer specs. Specify the location of your Riak installation in 'spec/support/test_server.yml. See warning e.inspect"
-        puts e.inspect
-        test_server = nil
-      end
+    begin
+      config = YAML.load_file('spec/support/test_server.yml')
+      $riak_test_server = Riak::TestServer.create(config.symbolize_keys)
+      $riak_test_server.start
+    rescue => e
+      warn "Can't run Riak::TestServer specs. Specify the location of your Riak installation in 'spec/support/test_server.yml. See warning e.inspect"
     end
   end
 
   config.after :suite do
-    FileUtils.rm_rf(File.expand_path('../../uploads',     __FILE__))
+    FileUtils.rm_rf(File.expand_path('../../uploads', __FILE__))
+    $riak_test_server.stop
+    puts "Stoping test server..." 
+    $riak_test_Server = nil
     FileUtils.rm_rf(File.expand_path('../test_server', __FILE__))
   end
 
